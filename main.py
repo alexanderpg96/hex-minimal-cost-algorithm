531353345f33939c19c42ba4535f7448982dc21e
Alexander Pearson-Goulart <pearsongoulart@gmail.com>
2018-04-27 11:42:04 -0700

    Added outline for main python file

---
+++ main.py
@@ -0,0 +1,11 @@
+# Put the index numbers into an array
+
+# Put the cost numbers into an array
+
+# get neighbors for index; returns neighbors and actions
+
+# get distance from index to finish (distance to goal); returns a number 1 - 10
+
+# print out the indices of the shortest path
+
+# print out the total minimal cost of the path
